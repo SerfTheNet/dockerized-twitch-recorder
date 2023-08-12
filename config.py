8af579291acbaeb50477d389dba8cc669e7c9a15
SerfTheNet <S.Lychko@mail.ru>
2023-08-12 17:19:21 +0300

    Filebrowser support added. Now there is single config point (.env file)

--- config.py
+++
@@ -1,4 +0,0 @@
-username = "STREAMER_USERNAME_HERE"
-client_id = "YOUR_TWITCH_APP_CLIENT_ID"
-root_path = '.'
-client_secret = "YOUR_TWITCH_APP_CLIENT_SERCET"
---
+++ configs/config.py
@@ -0,0 +1,6 @@
+from os import getenv
+
+username = getenv('TWITCH_USER_NAME')
+client_id = getenv('TWITCH_APP_CLIENT_ID')
+client_secret = getenv('TWITCH_APP_CLIENT_SERCET')
+root_path = '/twitch-recordings'
